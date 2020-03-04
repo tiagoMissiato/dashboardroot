3163917fc9fb5361198e9de08b6d916712813f3c
u001710 <tiago.rigoleto@xpi.com.br>
2020-03-04 16:48:22 -0300

    wip

--- app/src/main/java/br/com/xpinc/mobile/dashboardroot/MainActivity.kt
+++ app/src/main/java/br/com/xpinc/mobile/dashboardroot/MainActivity.kt
@@ -2,11 +2,15 @@ package br.com.xpinc.mobile.dashboardroot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.xpinc.mobile.featuremodule12.LoggerModule1
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loggerTest.text = LoggerModule1.logModule1()
     }
 }
